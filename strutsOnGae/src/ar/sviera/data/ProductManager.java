4ee9fe9be0ae02d0e2f801693ceed6bdb890e3f2
sergioviera <sergioviera@gmail.com>
2013-09-20 20:07:04 -0300

    try to commit on github again

--- strutsOnGae/src/ar/sviera/data/ProductManager.java
+++ strutsOnGae/src/ar/sviera/data/ProductManager.java
@@ -4,12 +4,9 @@ import javax.jdo.PersistenceManager;
 
 import ar.sviera.domain.Product;
 
-
 public class ProductManager {
-	
-	public static  void save(Product prod) {
+	public static void save(Product prod) {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		pm.makePersistent(prod);
 	}
 }
-
